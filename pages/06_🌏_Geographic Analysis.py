12d7168cc808439f52ec4937de6256820f742862
Yash Jain <jyash237@gmail.com>
2022-07-04 11:40:43 +0200

    final update

--- pages/06_🌏_Geographic Analysis.py
+++ pages/06_🌏_Geographic Analysis.py
@@ -6,12 +6,13 @@ df1 = pd.read_csv('dataset/DataCoSupplyChainDataset.csv',encoding='ISO-8859-1')
 
 
 st.title('Profit by Country')
-df5 = df1.groupby([ 'Order Country', 'Order City'])['Order Profit Per Order'].sum().reset_index(name='Profit of Orders').sort_values(by= 'Profit of Orders', ascending= True)
+df5 = df1.groupby([ 'Order Country', 'Order City'])['Order Profit Per Order'].sum().reset_index(name='Order Profit Per Order').sort_values(by= 'Order Profit Per Order', ascending= True)
 chart_5 = px.choropleth(df5,locationmode='country names', locations='Order Country',
-                    color='Profit of Orders',
+                    color='Order Profit Per Order',
                     hover_name='Order Country',
                     color_continuous_scale=px.colors.sequential.Plasma)
 st.write(chart_5)
+st.dataframe(df5)
 st.write('Profits mainly come from countries such as Indonesia, Australia, Argentina, Nicaragua, and Honduras.')
 st.title('Countries with highest Fraud')
 data2=df1[df1['Order Status'] == 'SUSPECTED_FRAUD']
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,18 @@ import streamlit as st
 import pandas as pd
 st.title('Supply Chain Data Analysis')
 
-st.markdown('Dataset')
+with st.expander("Developed By", expanded=False):
+    st.write("- Yash Kumar Jain ")
+    st.write("- Thi truc linh Le")
+st.write("A DataSet of Supply Chains used by the company DataCo Global was used for the analysis. Dataset of Supply "
+         "Chain ,Areas of important registered activities : Provisioning , Production , Sales , Commercial "
+         "Distribution.It also allows the correlation of Structured Data with Unstructured Data for knowledge "
+         "generation.")
+
+st.write('Type Data :')
+st.write('Structured Data : DataCoSupplyChainDataset.csv')
+st.write('Unstructured Data : tokenizedaccesslogs.csv (Clickstream)')
+
+st.write("Types of Products : Clothing , Sports , and Electronic Supplies")
 df1 = pd.read_csv('dataset/DataCoSupplyChainDataset.csv',encoding='ISO-8859-1')
 st.dataframe(df1)
